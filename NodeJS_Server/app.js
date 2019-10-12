60d18c9e1123f1ca323084e8465cce5658caed37
Andrew Wang <stonefish1089@gmail.com>
2019-10-12 03:12:18 -0400

    Created separate api's to query, added ability to insert into database

--- NodeJS_Server/app.js
+++ NodeJS_Server/app.js
@@ -9,8 +9,9 @@ client.connect();
 var app = express();
 app.set('port', process.env.PORT || 4000);
 
+/*Select kates fridge, pass back to ReactNative*/
 app.get('/', function (req, res, next) {
-	console.log("Queried:");
+	console.log("Selected:");
     client.query('SELECT * FROM fridge where name = $1', ['Kates Fridge'], function (err, result) {
         if (err) {
             console.log(err);
@@ -21,6 +22,21 @@ app.get('/', function (req, res, next) {
         console.log(query_result);
     });
 });
+
+/*Insert an entity into the fridge table*/
+app.get('/add', function (req, res, next) {
+	console.log("Inserted:");
+    client.query('insert into fridge(name) values($1)', ['James Fridge'], function (err, result) {
+        if (err) {
+            console.log(err);
+            res.status(400).send(err);
+        }
+        var query_result = result.rows[0];
+        res.status(200).send(query_result);
+        console.log(query_result);
+    });
+});
+
 
 app.listen(4000, function () {
     console.log('Server is running.. on Port 4000');
