9c7e1aa9d64623888b801bcbe393c4164a585d00
Andrew Wang <stonefish1089@gmail.com>
2019-10-12 14:39:56 -0400

    Scripts for initializing database tables and populating with initial food entities

--- PostgreSQL_Scripts/fridge_database_init.sql
+++ PostgreSQL_Scripts/fridge_database_init.sql
@@ -8,10 +8,10 @@ CREATE TABLE Fridge (
 
 CREATE TABLE Food_Items (
 	name varchar (50),/*Primary key not null*/
-	fridge_id serial NOT NULL,
-	exp_date date,/*Auto set to 0 if not specified by user...?*/
 	amnt int NOT NULL CHECK(amnt >= 0),
 	quant_units varchar (50) NOT NULL,
+	exp_date date,/*Auto set to 0 if not specified by user...?*/
+	fridge_id serial NOT NULL,
 	FOREIGN KEY (fridge_id) references Fridge(ID),
 	PRIMARY KEY (name, exp_date)
 );
@@ -29,4 +29,8 @@ INSERT INTO Fridge(name) VALUES('Kates Fridge');
 SELECT lastval() INTO last_id;
 
 INSERT INTO Food_Items VALUES('apple', 1, 'milliseconds', '2019-10-11', (SELECT * from last_id)); 
+*/
+
+/*
+@TODO: adding serial id's always increments; when deleting, change serial? lastval?
 */
