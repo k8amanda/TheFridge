632af4c39ac8701e1da871e52baf130498cefc9d
Andrew Cervantes <ajcervantes99@gmail.com>
2019-10-11 23:42:38 -0400

    Fridge.js with basic list

--- Fridge.js
+++ Fridge.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
-import { Alert, TouchableOpacity, Image, Text, View, StyleSheet } from 'react-native';
+import { Alert, TouchableOpacity, Image, Text, View, StyleSheet, TextInput } from 'react-native';
 import { Button } from 'react-native';
 
 export default class Fridge extends Component {
+  constructor() {
+    super();
+    this.state = {
+      size: 5,
+      names: [1,2,3,4,5],
+      dates: ["11/11/11", "11/11/12", "11/11/13", "11/11/14", "11/11/15"],
+   };
+  }
+  renderList = () =>{
+    let table = [];
+
+    for (let i = 0; i < this.state.size; i++) {
+        table.push(
+          <View key={i} style={styles.todoItem}>
+
+            <TextInput style={{ flex: 1 }} />
+              <Text style={styles.list}>√{this.state.names[i]}     {this.state.dates[i]}</Text>
+          </View>
+        );
+      }
+      return table;
+  };
   render() {
-    const data = [1,2,3,4,5];
-    const dates = ["11/11/11", "11/11/12", "11/11/13", "11/11/14", "11/11/15"];
     return (
       <View style={styles.container}>
         <View style={styles.header}>
@@ -13,22 +33,23 @@ export default class Fridge extends Component {
             <Image style={styles.imagef1} source={require('./Fridge-condensed-white.png')} />
           </TouchableOpacity>
            <View style={styles.leftHold} />
-        
+
           <Image style={styles.imagef2} source={require('./Fridge-extended.png')} />
           <View style={styles.rightHold} />
         </View>
         <View style={styles.buttonContainer}>
           <Text>Hello, world!</Text>
+          {this.renderList()}
         </View>
         <View style={styles.footer}>
           <TouchableOpacity onPress={this.handlePress}>
-            <Image style={styles.imagef1} source={require('./Fridge-condensed-white.png')} />
+            <Image style={styles.imagef1} source={require('./Add-button.png')} />
           </TouchableOpacity>
           <TouchableOpacity onPress={this.handlePress}>
             <Image style={styles.imagef1} source={require('./The_Fridge-icon.png')} />
           </TouchableOpacity>
           <TouchableOpacity onPress={this.handlePress}>
-            <Image style={styles.imagef1} source={require('./Fridge-condensed-white.png')} />
+            <Image style={styles.imagef1} source={require('./Compost-button.png')} />
           </TouchableOpacity>
 
 
@@ -41,6 +62,9 @@ export default class Fridge extends Component {
   }
 }
 const styles = StyleSheet.create({
+  list:{
+
+  },
   leftHold:{
     width: 50,
   },
